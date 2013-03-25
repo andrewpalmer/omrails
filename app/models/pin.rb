8984650ebfdd381880812a84e7452f80efde821a
Andrew Palmer <adpalmer01@gmail.com>
2013-03-25 20:50:40 +1100

    Add pins associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,10 @@
 class Pin < ActiveRecord::Base
   attr_accessible :description
 
-  validates :description, presence: true 
+  validates :description, presence: true
+
+  #test for git hub 
+
+  belongs_to :user
+  validates :user_id, presence: true
 end
